a3bc1f0ea06892b2311777053f96b1b10a823cd1
AadityaMohit <150881374+AadityaMohit@users.noreply.github.com>
2025-05-03 09:18:02 +0530

    added all origin allowed

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
-
+console.log("testing");
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
