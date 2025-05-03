a5e97bf2e3c5326296bf0289d8a100e520f04460
AadityaMohit <150881374+AadityaMohit@users.noreply.github.com>
2025-05-03 09:15:32 +0530

    added all origin allowed

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 
 connectDB();
-app.use(cors());
+
+app.use(cors({
+  origin: '*',
+}));
+
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
